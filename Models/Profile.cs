86b18163818e3c9c8b182bd31eb21548e9d86d02
Vladislav East <vladislav.sage@yandex.ru>
2025-01-10 19:13:36 +0300

    Добавление моделей User и Profile

---
+++ Models/Profile.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace dotNetCources.Models
+{
+	public class Profile
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public string UserId { get; set; }
+
+		[ForeignKey("UserId")]
+		public virtual User User { get; set; }
+
+		[MaxLength(100)]
+		public string FullName { get; set; }
+
+		[MaxLength(100)]
+		public string Country { get; set; }
+
+		public string About { get; set; }
+
+		[MaxLength(255)]
+		public string Image { get; set; } = "default-user.jpg";
+
+		[Required]
+		public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+		public override string ToString()
+		{
+			return !string.IsNullOrEmpty(FullName) ? FullName : User.FullName;
+		}
+
+		public void InitializeDefaults()
+		{
+			if (string.IsNullOrEmpty(FullName))
+				FullName = User?.UserName;
+		}
+	}
+}
---
+++ Models/User.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace dotNetCources.Models
+{
+	public class User : IdentityUser 
+	{
+		[Required]
+		[MaxLength(100)]
+		public override string UserName { get; set; }
+
+		[Required]
+		[MaxLength(100)]
+		public override string Email { get; set; }
+
+		[MaxLength(100)]
+		public string FullName { get; set; }
+
+		[MaxLength(100)]
+		public string OTP { get; set; }
+
+		[MaxLength(1000)]
+		public string RefreshToken { get; set; }
+
+		public Profile Profile { get; set; }
+
+		public override string ToString()
+		{
+			return Email;
+		}
+
+		public void InitializeDefaults()
+		{
+			var emailUsername = Email.Split('@')[0];
+
+			if (string.IsNullOrEmpty(FullName))
+				FullName = emailUsername;
+
+			if (string.IsNullOrEmpty(UserName))
+				UserName = emailUsername;
+		}
+
+	}
+}
