17670128e531aa0c6b042983f7a22d7f866d4f45
Vladislav East <vladislav.sage@yandex.ru>
2025-01-13 20:04:11 +0300

    RefreshToken and OTP IsNull

--- Models/User.cs
+++ Models/User.cs
@@ -18,10 +18,10 @@ namespace dotNetCources.Models
 		public string FullName { get; set; }
 
 		[MaxLength(100)]
-		public string OTP { get; set; }
+		public string? OTP { get; set; }
 
 		[MaxLength(1000)]
-		public string RefreshToken { get; set; }
+		public string? RefreshToken { get; set; }
 
 		public Profile Profile { get; set; }
 
